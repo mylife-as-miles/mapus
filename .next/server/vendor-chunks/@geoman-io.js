f98a9c7b5f7f00d7fd29da5261907427372f91d2
mylife-as-miles <mylife-as-miles@users.noreply.github.com>
2025-11-20 13:09:21 +0100

    Enhance room management in db.js and main.js by refining createRoom, getRoom, and updateRoom methods. Replace Firebase calls with MapusDB methods for improved data handling and local storage support.

---
+++ .next/server/vendor-chunks/@geoman-io.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@geoman-io";
+exports.ids = ["vendor-chunks/@geoman-io"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css":
+/*!*****************************************************************************!*\
+  !*** ./node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css ***!
+  \*****************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"c797fef8839e\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvQGdlb21hbi1pby9sZWFmbGV0LWdlb21hbi1mcmVlL2Rpc3QvbGVhZmxldC1nZW9tYW4uY3NzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxjQUFjO0FBQzdCLElBQUksS0FBVSxFQUFFLEVBQXVCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWFwdXMvLi9ub2RlX21vZHVsZXMvQGdlb21hbi1pby9sZWFmbGV0LWdlb21hbi1mcmVlL2Rpc3QvbGVhZmxldC1nZW9tYW4uY3NzP2QzMmMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJjNzk3ZmVmODgzOWVcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css\n");
+
+/***/ })
+
+};
+;
